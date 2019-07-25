d4152b6ef1ac50be0d0fd3a62e0ea055a82e8b9a
Sarah Tatro <statrox4@gmail.com>
2019-07-24 19:00:48 -0600

    Model tests

---
+++ spec/models/course_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Course do
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+  end
+
+  describe 'relationships' do
+    it {should have_many(:course_students)}
+    it {should have_many(:students).through(:course_students)}
+  end
+end 
---
+++ spec/models/student_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper' 
+
+RSpec.describe Student do
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+  end
+
+  describe 'relationships' do
+    it {should have_many(:course_students)}
+    it {should have_many(:courses).through(:course_students)}
+  end
+end
