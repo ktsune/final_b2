4274d4d51a32f438997c8269b7ce560a8eb0291a
Sarah Tatro <statrox4@gmail.com>
2019-07-24 21:12:28 -0600

    Set up show spec for students

--- spec/features/student/show_spec.rb
+++ spec/features/student/show_spec.rb
@@ -2,8 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'User' do
   describe 'I visit a student show page' do
-    it 'I see student name, courses, and grades for each course' do
-      
+    before :each do
+      @peregrine = Student.create!(name: "Peregrine")
+      @mulan = Student.create!(name: "Mulan")
+      visit student_path(@peregrine)
     end
-  end
+
+    it 'I see student name' do
+
+      expect(page).to have_content(@peregrine.name)
+    end
+
+    it 'I can see student courses, and grades for each course' do
+
+    end
+  end 
 end
