f78dfdde6c7b5fdaed77dcd40b1837e5a80a6c14
Sarah Tatro <statrox4@gmail.com>
2019-07-25 16:06:09 -0600

    View for course index

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,6 +2,10 @@ class CoursesController < ApplicationController
 
   def index
     @courses = Course.all
+    # binding.pry
+    @students = @courses.map do |course|
+      course.students
+    end 
   end
 
 end
