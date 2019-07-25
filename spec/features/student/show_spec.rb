ab33dc1f6348ee4689dd96b158cfae4b2ca725d1
Sarah Tatro <statrox4@gmail.com>
2019-07-24 19:52:18 -0600

    Create seeds

---
+++ spec/features/student/show_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'User' do
+  describe 'I visit a student show page' do
+    it 'I see student name, courses, and grades for each course' do
+      
+    end
+  end
+end
