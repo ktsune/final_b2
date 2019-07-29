8265133e189bab2a498a2cc5107c20344148359c
Sarah Tatro <statrox4@gmail.com>
2019-07-29 12:33:03 -0600

    Add functionality to enroll a student in a course

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
     @courses = @student.courses
   end
+
+  def enroll
+    @student = Student.find(params[:id])
+    @course = Course.find(params[:course_id])
+    @student.student_courses.create!(course_id: params[:course_id])
+
+    redirect_to student_path(@student.id)
+  end
 end
